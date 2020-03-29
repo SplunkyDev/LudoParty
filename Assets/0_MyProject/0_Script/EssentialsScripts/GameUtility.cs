59f3fde24e8dbc59af99c966473d2991dd1a80b1
DevJeeth <konerirajb@yahoo.com>
2020-03-29 17:09:02 +0530

    +Essentials

---
+++ Assets/0_MyProject/0_Script/EssentialsScripts/GameUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using UnityEngine;
+
+namespace GameUtility.Base
+{
+
+
+    public interface ISingletonCreatedListener
+    {
+        void OnInstanceCreated();
+    }
+
+    public interface IEventBase
+    {
+
+    }
+
+    public interface IInputEvent : IEventBase
+    {
+
+    }
+
+    public interface IEventListener
+    {
+        void RegisterForEvents();
+        void DeRegisterForEvents();
+    }
+
+
+}
+
